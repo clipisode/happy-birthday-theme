815ef522e2ba07dd27a4d6c77a41f3248b24cc78
Max Schmeling <max@clipisode.com>
2021-05-12 11:28:50 -0500

    Add single video element

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-export function getVideoComposition() {
-  return {
-    videos: []
-  }
-}
-
-export function getElements() {
-  return [];
+export function getElements(answer) {
+  return [
+    {
+      type: "video",
+      name: "video.reply",
+      videoKey: answer.reply.clip.id,
+      source: "clip",
+      startAt: 2,
+      endAt: 2 + answer.reply.clip.duration,
+      props: {
+        x: 0,
+        y: 0,
+        width: 720,
+        height: 1280,
+      },
+    }
+  ];
 }
